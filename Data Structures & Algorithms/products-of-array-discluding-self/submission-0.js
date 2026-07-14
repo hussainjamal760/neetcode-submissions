a7e8e11495cd54c283e3fb02f5cf4550297f8de9
Hussain Jamal <hjamal9865@gmail.com>
2026-07-14 17:59:29 +0500

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    productExceptSelf(nums) {
+        let result =  new Array(nums.length).fill(1)
+        let left =1; 
+        for(let i = 0; i<nums.length;i++){
+            result[i] = left;
+            left *= nums[i]
+        }
+
+        let right = 1;
+        for(let i = nums.length-1 ; i>=0 ; i--){
+            result[i] *= right;
+            right *= nums[i]
+        }
+
+        return result;
+    }
+}
